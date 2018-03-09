c7f47f9981550fb1601362329a90a64c0cc43361
Vitalii Dikhtiaruk <vitaliiwt@gmail.com>
2018-03-09 11:44:10 +0200

    added hw lesson 4

---
+++ src/lesson4/task1.java
@@ -0,0 +1,14 @@
+package lesson4;
+
+public class task1 {
+	public static void main(String[] args) {
+//		System.out.println(Math.PI);
+		
+		System.out.format("%.1f%n", Math.PI);		
+		System.out.format("%.2f%n", Math.PI);		
+		System.out.format("%.3f%n", Math.PI);		
+		System.out.format("%.4f%n", Math.PI);
+		System.out.format("%.5f%n", Math.PI);		
+	}
+
+}
---
+++ src/lesson4/task2.java
@@ -0,0 +1,47 @@
+package lesson4;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class task2 {
+	public static void main(String[] args) {
+		int[] a = { 1, 2, 3, -6, 10, -11 };
+		System.out.println(maxArray(a));
+		int b = 3;
+		int c = 3;
+		char[][] array = new char[b][c];
+		square(array, b, c);
+		String s = "abc ab   1 abas";
+		System.out.println(wordCounter(s));
+
+	}
+
+	public static int maxArray(int[] arr) {
+		Arrays.sort(arr);
+		return arr[arr.length - 1];
+	}
+
+	public static char[][] square(char[][] arr, int a, int b) {
+
+		for (int i = 0; i < a; i++) {
+			for (int j = 0; j < b; j++) {
+				arr[i][j] = '+';
+				System.out.print(arr[i][j]);
+			}
+			System.out.println();
+		}
+		return arr;
+
+	}
+
+	public static int wordCounter(String s) {
+		int c = 0;
+		Matcher matcher = Pattern.compile("(\\s+)?\\S+(\\s*)?").matcher(s);
+		while (matcher.find()) {
+			c++;
+		}
+		return c;
+	}
+
+}
